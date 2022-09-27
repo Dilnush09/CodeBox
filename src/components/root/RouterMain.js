427b517408e1148b632dbee6dac03acc25991806
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2022-09-27 14:20:01 +0500

    de
    de

---
+++ src/components/root/RouterMain.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RouterMain() {
+  return (
+    <div>RouterMain</div>
+  )
+}
---
+++ src/components/util/navbarMain.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function navbarMain() {
+  return (
+    <div>navbarMain</div>
+  )
+}
